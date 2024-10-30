f7f17b3987b6d3927c84e1a7cfc99ecdf5b72187
Joshua Ordehi <45109738+ordehi@users.noreply.github.com>
2024-10-30 16:05:31 -0400

    remove contact form from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 import Button from '@/components/Button';
-import ContactForm from '@/components/ContactForm';
-import { contactFormConfig } from '@/config/components/contactForm.client';
 import { FiArrowRight, FiPhone, FiMail } from 'react-icons/fi';
 
 export default function Home() {
@@ -57,11 +55,6 @@ export default function Home() {
           <Button variant='secondary' size='md' icon={<FiMail />}>
             Contact Sales
           </Button>
-        </div>
-
-        {/* Contact Form Section */}
-        <div className='mt-12 w-full max-w-2xl'>
-          <ContactForm config={contactFormConfig} />
         </div>
       </main>
     </div>
